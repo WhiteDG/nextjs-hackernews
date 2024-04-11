dc4116ac1dee24b6b0d042170ea6a6253d0550a7
Wh1te <White.hcj@gmail.com>
2024-04-11 14:52:17 +0800

    Disallow robots

--- app/robots.ts
+++ app/robots.ts
@@ -5,8 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: '*',
-        allow: ['/'],
-        disallow: ['/login', '/sign'],
+        disallow: ['/']
       }
     ]
   }
